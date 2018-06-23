4499df06e07b7d1286221298ba938caf7492a3cc
SHIVANG CHOPRA <shivangchopra11@gmail.com>
2018-06-23 11:36:55 +0530

    Class 1 online

---
+++ RollABallOnline/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody player;
+
+
+	void Start () {
+		player = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+		float moveHoriz = Input.GetAxis("Horizontal");
+		float moveVer = Input.GetAxis("Vertical");
+		Vector3 move = new Vector3(moveHoriz, 0, moveVer);
+		player.AddForce(move);
+	}
+}
